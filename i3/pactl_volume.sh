1641fefaace507fcc65dd25737d427a834c3bc19
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2020-09-07 14:09:06 +0200

    first commit of the i3 folder

---
+++ i3/pactl_volume.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+EXIST="$(pacmd list-sinks | grep name: | grep <BLUETOOH SINK>)"
+if [ -z "$EXIST" ]; then
+    pactl set-default-sink '<INTERNAL SINK>'
+else
+    pactl set-default-sink '<BLUETOOH SINK>'
+fi
